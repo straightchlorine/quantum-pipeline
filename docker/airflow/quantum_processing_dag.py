bf1bac4c178abc9c4ab1125d11000e1a7f03994e
Piotr Krzysztof <piotrlis555@gmail.com>
2025-04-15 13:09:20 +0200

    refactor(airflow): tidied up the dag

--- docker/airflow/quantum_processing_dag.py
+++ docker/airflow/quantum_processing_dag.py
@@ -13,7 +13,6 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.models import Variable
-from airflow.operators.python import ShortCircuitOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from airflow.utils.email import send_email
 from airflow.utils.log.logging_mixin import LoggingMixin
@@ -21,12 +20,7 @@ from airflow.utils.log.logging_mixin import LoggingMixin
 logger = LoggingMixin().log
 
 try:
-    from scripts.quantum_incremental_processing import (
-        DEFAULT_CONFIG,
-        check_for_new_data,
-        create_spark_session,
-        list_available_topics,
-    )
+    from scripts.quantum_incremental_processing import DEFAULT_CONFIG
 except ImportError:
     logger.error(
         "Could not import the quantum processing module. Make sure it's in the PYTHONPATH."
@@ -43,66 +37,6 @@ default_args = {
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
-
-
-def check_for_data_to_process(**kwargs):
-    """
-    Check for new data to process in quantum data topics
-
-    Uses the check_for_new_data function from the quantum_incremental_processing module to
-    check if there are new experiment results that need to be processed.
-    """
-    config = kwargs['config']
-    logger.info(f'Creating Spark session with config: {config}')
-
-    # create a Spark session
-    spark = create_spark_session(config)
-
-    logger.info(f'Spark session created - AppName: {spark.conf.get("spark.app.name")}')
-    logger.info(f'Spark version: {spark.version}')
-    logger.info(f'Spark master: {spark.conf.get("spark.master")}')
-    logger.info(f'Spark driver host: {spark.conf.get("spark.driver.host")}')
-    logger.info(
-        f'Spark warehouse path: {spark.conf.get("spark.sql.catalog.quantum_catalog.warehouse")}'
-    )
-    logger.info(f'S3 endpoint: {spark.conf.get("spark.hadoop.fs.s3a.endpoint")}')
-
-    logger.info(f'Driver memory: {spark.conf.get("spark.driver.memory", "default")}')
-    logger.info(f'Executor memory: {spark.conf.get("spark.executor.memory", "default")}')
-    logger.info(f'Executor cores: {spark.conf.get("spark.executor.cores", "default")}')
-    logger.info(f'Executor instances: {spark.conf.get("spark.executor.instances", "default")}')
-    logger.info(
-        f'Dynamic allocation enabled: {spark.conf.get("spark.dynamicAllocation.enabled", "default")}'
-    )
-
-    try:
-        bucket_path = config.get('S3_BUCKET', DEFAULT_CONFIG['S3_BUCKET'])
-        available_topics = list_available_topics(spark, bucket_path)
-
-        if not available_topics:
-            logger.info('No topics found in the bucket')
-            return False
-
-        has_new_data = False
-        topics_with_data = []
-
-        # check each topic for new data
-        for topic in available_topics:
-            logger.info(f'Checking topic: {topic}')
-            _, df = check_for_new_data(spark, topic, config)
-
-            if df is not None and not df.isEmpty():
-                has_new_data = True
-                topics_with_data.append(topic)
-                logger.info(f'Found new data for topic: {topic}')
-
-        # store information about which topics have data
-        kwargs['ti'].xcom_push(key='topics_with_data', value=topics_with_data)
-
-        return has_new_data
-
-    finally:
-        spark.stop()
 
 
 # function to send success email with processing results
@@ -126,34 +60,26 @@ def send_success_email(context):
     send_email(to=default_args['email'], subject=subject, html_content=html_content)
 
 
+for k, v in DEFAULT_CONFIG.items():
+    if Variable.get(k, default_var=None) is None:
+        Variable.set(k, v)
+
+Variable.set('MINIO_ACCESS_KEY', os.getenv('MINIO_ACCESS_KEY'))
+Variable.set('MINIO_SECRET_KEY', os.getenv('MINIO_SECRET_KEY'))
+
 # create the DAG
 with DAG(
     'quantum_feature_processing',
-    default_args=default_args,
-    description='Process quantum experiment data into feature tables',
-    schedule_interval=timedelta(hours=1),  # Run hourly
-    start_date=datetime(2023, 1, 1),
+    default_args={**default_args, 'retries': 3, 'retry_delay': timedelta(minutes=20)},
+    description='Process quantum experiment data into ML feature tables',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2025, 1, 1),
     catchup=False,
-    tags=['quantum', 'features', 'processing'],
+    tags=['quantum', 'ML', 'features', 'processing', 'Apache Spark'],
     on_success_callback=send_success_email,
 ) as dag:
-    # set default config values
-    for k, v in DEFAULT_CONFIG.items():
-        Variable.set(k, v)
-
-    Variable.set('MINIO_ACCESS_KEY', os.getenv('MINIO_ACCESS_KEY'))
-    Variable.set('MINIO_SECRET_KEY', os.getenv('MINIO_SECRET_KEY'))
-
-    # check for new data using the imported functions
-    check_for_data = ShortCircuitOperator(
-        task_id='check_for_data',
-        python_callable=check_for_data_to_process,
-        op_kwargs={'config': DEFAULT_CONFIG},
-        provide_context=True,
-    )
-
     # submit Spark job - only run if check_for_data returns True
-    run_quantum_processing = SparkSubmitOperator(
+    quantum_simulation_results_processing = SparkSubmitOperator(
         task_id='run_quantum_processing',
         application='/opt/airflow/dags/scripts/quantum_incremental_processing.py',
         conn_id='spark_default',
@@ -187,5 +113,4 @@ with DAG(
         verbose=True,
     )
 
-    # task dependencies
-    check_for_data >> run_quantum_processing
+    quantum_simulation_results_processing
