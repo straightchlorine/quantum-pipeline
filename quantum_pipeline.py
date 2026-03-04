b63493842572d313a3ad18fde8a7bd79024399c4
Piotr Krzysztof <piotr@codextechnologies.org>
2026-03-04 16:00:15 +0100

    fix: entry point properly handles convergnce/max_iterations

--- quantum_pipeline.py
+++ quantum_pipeline.py
@@ -2,6 +2,7 @@
 
 import os
 
+from quantum_pipeline.configs.defaults import DEFAULTS
 from quantum_pipeline.configs.parsing.argparser import QuantumPipelineArgParser
 from quantum_pipeline.runners.vqe_runner import VQERunner
 from quantum_pipeline.utils.logger import get_logger
@@ -11,10 +12,13 @@ logger = get_logger('QuantumPipeline')
 
 def execute_simulation(**kwargs):
     threshold = None
+    max_iterations = kwargs['max_iterations']
     apply_threshold = kwargs.get('convergence')
     if apply_threshold:
         threshold = kwargs['threshold']
         logger.info(f'Applying convergence threshold {threshold} during minimization')
+        if max_iterations == DEFAULTS['max_iterations']:
+            max_iterations = None
 
     if os.getenv('KAFKA_SERVERS', None):
         kwargs['kafka_config'].servers = os.getenv('KAFKA_SERVERS')
@@ -22,7 +26,7 @@ def execute_simulation(**kwargs):
     runner = VQERunner(
         filepath=kwargs['file'],
         basis_set=kwargs['basis'],
-        max_iterations=kwargs['max_iterations'],
+        max_iterations=max_iterations,
         convergence_threshold=threshold,
         optimizer=kwargs['optimizer'],
         ansatz_reps=kwargs['ansatz_reps'],
